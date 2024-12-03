1370b1a9fa2af111f68718640d58baa9c4c36e12
Sudip Biswas <sudipbiswas2142@gmail.com>
2024-12-04 01:14:48 +0530

    basic implementation of text-splitting

---
+++ src/utils/text-splitting.ts
@@ -0,0 +1,29 @@
+import { Doc } from "../types";
+// import { ELEPHANT_WIKI } from "./mock-data";
+
+// Splits the text into chunks of a specified size
+// Useful for splitting the text into smaller chunks for better context
+export async function splitText(data: Doc[]) {
+  const chunks: Doc[] = [];
+
+  const CHUNK_SIZE = 500;
+
+  for (const { content, name } of data) {
+    const words = content.split(/\s+/);
+
+    for (let i = 0; i < words.length; i += CHUNK_SIZE) {
+      const chunkWords = words.slice(i, i + CHUNK_SIZE);
+      chunks.push({ content: chunkWords.join(" "), name });
+    }
+  }
+
+  return chunks;
+}
+
+// async function main() {
+//   const chunks = await splitText([{ content: ELEPHANT_WIKI, name: "Elephants" }]);
+//   console.log(chunks);
+//   console.log(chunks.length);
+// }
+
+// main();
